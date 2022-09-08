c59659a69c74523f5d495394b672d4263da2f1be
Alexander <ermakovich@km.ru>
2022-09-08 13:31:03 +0700

    HomeWork001 is finish!

---
+++ Documents/Обучение ИТ/C#/Seminars/Seminar002/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+int FindBiggerDigit(int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+
+    int max;
+    if(ed > dec) max = ed;
+    else max = dec; 
+
+    return max;
+}
+
+int randNumber = new Random().Next(10, 100);
+int biggerDigit = FindBiggerDigit(randNumber);
+Console.WriteLine($"Bigger digit of {randNumber} is {biggerDigit}");
+*/
+
+int MakeNewDigit(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+
+    int newNumber = sot * 10 + ed;
+
+    return newNumber;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int newDigit = MakeNewDigit(randNumber);
+Console.WriteLine($"RandomNumber is {randNumber} New digit is {newDigit}");
